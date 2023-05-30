ee4c77a82246d4130744ddddaed0f02555450416
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-05-31 01:17:54 +0600

    categery wise data displayed

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,22 +1,34 @@
 
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import Heading from "../Heading/Heading";
 import Container from "../Shared/Container";
 import Loader from "../Shared/Loader";
 import Card from "./Card";
 
 
 const Rooms = () => {
+    const [params , setParams] = useSearchParams();
+    const category = params.get('category');
+    console.log(category)
     const [rooms, setRooms] = useState([]);
     const [loading, setLoading] = useState(false);
     useEffect(() => {
         setLoading(true);
         fetch('./rooms.json')
             .then(res => res.json())
-            .then(data => {setRooms(data)
+            .then(data => {
+                if(category){
+                   const filtered= data.filter(room => room.category === category)
+                     setRooms(filtered) 
+                }
+                else{
+                    setRooms(data)
+                }
             setLoading(false);
     })
             .catch(err => console.log(err.message));  
-    }, [])
+    }, [category])
 
     if (loading) {
         return <Loader></Loader>
@@ -24,11 +36,18 @@ const Rooms = () => {
 
     return (
         <Container>
-            <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            { rooms && rooms.length > 0 ? (<div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                 {rooms.map((room , i) => <Card
                 key={i}
                 room={room}></Card>)}
+            </div> ) : ( <div className="pt-12">
+                <Heading
+                title="No Rooms Available in this Category"
+                subtitle="Please select another category"
+                center={true} 
+                ></Heading>
             </div>
+            )}
         </Container>
     );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../Components/Shared/Footer/Footer"
 import Navbar from "../Components/Shared/Navbar/Navbar"
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
     <div>
       <Navbar/>
       <div className=" pt-28 pb-20"><Outlet></Outlet></div>
+      <Footer></Footer>
     </div>
   )
 }
